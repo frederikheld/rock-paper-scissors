8b5866c7d3364d12bea904dfa05707de215cda3b
Frederik Held <git@frederikheld.de>
2021-02-17 20:17:46 +0100

    Basic steps sketched out in comments

--- main.py
+++ main.py
@@ -2,4 +2,24 @@ import tkinter as tk
 
 window = tk.Tk()
 
+## Koordinatensystem --> Canvas initialisieren
+
+## Gegenstände  --> Grafik-Objekte auf dem Canvas initialisieren
+
+## Schleife
+
+    ## Gegenstände bewegen sich --> Algorithmus
+        ## erst mal random
+        ## später: gezielt auf das Opfer zu bewegen (Sichtfeld & gezielte Bewegung)
+
+    ## Kollisionserkennung
+
+    ## bei Kollision:
+        ## RPS Regeln anwenden
+        ## Verlierer wird umgewandelt in typ des Gewinner-Objekts
+
+    ## Wenn nur ein Typ übrig ist: schleife beenden
+    
+## Gewinner bekannt geben
+
 window.mainloop()
